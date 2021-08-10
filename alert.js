6c43fd689490f567f25129a9907043e91700d415
dlehdwoWkd <86726031+dlehdwoWkd@users.noreply.github.com>
2021-08-10 18:39:09 +0900

    variable and constant

--- alert.js
+++ alert.js
@@ -1,1 +1,16 @@
-alert( "I'm JavaScript" )
+"use strict";
+
+/* 1번과제
+let admin;
+let name = 'John';
+admin = name;
+
+alert(admin) */
+
+/* 2번과제
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+*/
+
+/* 3번과제
+birthday는 대문자로 바꾸기, age는 대문자 상수에 적합 x */
